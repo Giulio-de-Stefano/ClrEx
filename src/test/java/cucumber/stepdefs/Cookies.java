c97064bc4f381ea7ee03f7c67bba484f0508daef
Giulio-de-Stefano <padremaronno89@gmail.com>
2018-12-19 01:44:28 +0000

    Added TestRunner, basic step defs and first feature

---
+++ src/test/java/cucumber/stepdefs/Cookies.java
@@ -0,0 +1,24 @@
+package cucumber.stepdefs;
+
+import cucumber.World;
+import cucumber.api.java8.En;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class Cookies implements En {
+
+    public Cookies(World world) {
+
+        Then("^the cookies alert panel is displayed", () -> {
+            assertTrue(world.homePage.cookieAlertPanel.isDisplayed());
+        });
+
+        Then("^the cookies alert panel is not displayed", () -> {
+            assertFalse(world.homePage.cookieAlertPanel.isDisplayed());
+        });
+
+
+    }
+
+}
---
+++ src/test/resources/features/cookiesAlert.feature
@@ -0,0 +1,5 @@
+Feature: Cookies alert behaviour
+
+  Scenario: the cookie alert panel is displayed
+    When I navigate to the home page
+    Then the cookies alert panel is displayed
