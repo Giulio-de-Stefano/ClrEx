df9d93baa545c330fefc2fd58a35dd8321b701cd
Giulio-de-Stefano <padremaronno89@gmail.com>
2018-12-19 01:42:08 +0000

    Made pageAB attributes private

--- src/test/java/pages/PageAB.java
+++ src/test/java/pages/PageAB.java
@@ -3,8 +3,8 @@ package pages;
 import org.openqa.selenium.WebDriver;
 
 public class PageAB {
-    WebDriver driver;
-    String url = "https://www.clearscore.com";
+    private WebDriver driver;
+    private String url = "https://www.clearscore.com";
 
     public PageAB(WebDriver driver) {
         this.driver = driver;
